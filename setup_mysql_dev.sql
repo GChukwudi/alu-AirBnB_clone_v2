7cbfa5d924ebe56ffa7ef17692cb7726cabb551e
GChukwudi <g.chukwudi@alustudent.com>
2024-01-31 11:19:07 +0200

    Database Set up

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,9 @@
+-- Create database hbnb_dev_db
+-- Add new user hbnb_dev with password hbnb_dev_pwd in localhost
+-- Grant privileges to hbnb_dev
+
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
