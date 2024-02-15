56eee46890dab1737af27da0e6c27f305a07f736
GChukwudi <g.chukwudi@alustudent.com>
2024-02-15 17:58:16 +0200

    updated

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -10,8 +10,7 @@ mkdir -p /data/web_static/shared/
 echo "Holberton School" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-chown -R ubuntu /data/
-chgrp -R ubuntu /data/
+chown -R www-data:www-data /data/web_static/
 
 printf %s "server {
 	listen 80 default_server;
