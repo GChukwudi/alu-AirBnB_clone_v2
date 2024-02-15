43e47a119470007ab91f16923f8d2d975457af80
GChukwudi <g.chukwudi@alustudent.com>
2024-02-15 22:00:30 +0200

    updated

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,39 +3,36 @@
 Fabric script that distributes an archive to your web servers
 """
 import os
-from fabric.api import local, env, put, run
+from fabric.api import env, put, run
 from datetime import datetime
-
 
 # Setup Fabric environment variables.
 env.hosts = ['54.210.254.62', '54.227.96.209']
 env.user = 'ubuntu'
-    
+
 def do_deploy(archive_path):
     """
     Distributes an archive to your web servers
     """
-    if os.path.exists(archive_path):
-        archived_file = archive_path[9:]
+    if not os.path.exists(archive_path):
+        print("Archive file doesn't exist.")
+        return False
+
+    try:
+        archived_file = archive_path.split('/')[-1]
         new_version = '/data/web_static/releases/' + archived_file[:-4]
 
-        archived_file = '/tmp/' + archived_file
-        
         put(archive_path, '/tmp/')
         run('sudo mkdir -p {}'.format(new_version))
-
-        run('sudo tar -xzf {} -C {}/'.format(archived_file,
-                                        new_version))
-        run('sudo rm {}'.format(archived_file))
-        run('sudo mv {}/web_static/* {}/'.format(new_version,
-                                                 new_version))
-        
-        # Clean up old releases
+        run('sudo tar -xzf /tmp/{} -C {}'.format(archived_file, new_version))
+        run('sudo rm /tmp/{}'.format(archived_file))
+        run('sudo mv {}/web_static/* {}'.format(new_version, new_version))
         run('sudo rm -rf {}/web_static'.format(new_version))
         run('sudo rm -rf /data/web_static/current')
         run('sudo ln -s {} /data/web_static/current'.format(new_version))
 
         print('New version deployed!')
         return True
-    else:
+    except Exception as e:
+        print("Error deploying: {}".format(str(e)))
         return False
