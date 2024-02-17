02b281b44415665940908364dcbefc3b69c18848
God'sFavour Chukwudi <127259967+GChukwudi@users.noreply.github.com>
2024-02-17 11:41:25 +0200

    updated

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,37 +1,12 @@
 #!/usr/bin/env bash
-# Sets up a web server for deployment of web_static.
+# web static development
 
-apt-get update
-apt-get install -y nginx
-
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-
-echo "Holberton School" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-chown -R ubuntu:ubuntu /data/
-
-printf %s "server {
-	listen 80 default_server;
-	listen [::]:80 default_server;
-	add_header X-Served-By $hostname;
-	root /var/www/html;
-	location /hbnb_static {
-	    alias /data/web_static/current/;
-	    index index.html index.htm;
-	}
-	
-	location /redirect_me {
-        return 301 http://github.com/GChukwudi;
-	}
-	
-	error_page 404 /404.html;
-	location /404 {
-	    root /var/www/html;
-	    internal;
-	}
-}" > /etc/nginx/site-available/default
-
-ln -sf /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
-service nginx restart
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install nginx
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+echo "Hello, this is a test HTML file." | sudo tee /data/web_static/releases/test/index.html
+sudo rm -rf /data/web_static/current
+sudo ln -s /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i '44i \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/c
