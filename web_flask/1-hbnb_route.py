1d4026c2757e186eedb7fb3198df3976471f35e6
God'sFavour Chukwudi <127259967+GChukwudi@users.noreply.github.com>
2024-02-20 18:21:22 +0200

    updated

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -13,12 +13,13 @@ def index():
     """
     return "Hello HBNB!"
 
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """
     display HBNB
     """
     return "HBNB"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
