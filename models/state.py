87edf484a4775bd4c5671a12ecd5a86bcdd1acdc
God'sFavour Chukwudi <127259967+GChukwudi@users.noreply.github.com>
2024-02-22 21:57:45 +0200

    updated

--- models/state.py
+++ models/state.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 import models
-from models import storage
 from models.base_model import BaseModel, Base
 import os
 from sqlalchemy import Column, String
