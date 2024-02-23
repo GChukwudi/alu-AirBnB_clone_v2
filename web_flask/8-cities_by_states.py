464a0f9852de9fb1d8ccff6d64cab81ef4624b1d
God'sFavour Chukwudi <127259967+GChukwudi@users.noreply.github.com>
2024-02-23 14:13:54 +0200

    updated

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -4,23 +4,22 @@ script that starts a Flask web application"""
 from flask import Flask, render_template
 from models import storage
 from models.state import State
-from models.city import City
 app = Flask(__name__)
-
-
-@app.teardown_appcontext
-def close_session(exception):
-    """ close session """
-    storage.close()
 
 
 @app.route('/')
 def cities_by_states():
     """ display a HTML page: (inside the tag BODY) """
     states = storage.all(State)
-    sorted_stated = sorted(states, key=lambda k: k.name)
+    #sorted_stated = sorted(states, key=lambda k: k.name)
 
-    return render_template('8-cities_by_states.html', states=sorted_stated)
+    return render_template('8-cities_by_states.html', states=states)
+
+
+@app.teardown_appcontext
+def close_session(exception):
+    """ close session """
+    storage.close()
 
 
 if __name__ == '__main__':
--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""Starts a Flask web application"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def close_session(exception):
+    """Closes the session"""
+    storage.close()
+
+
+@app.route('/states', strict_slashes=False)
+def states():
+    """Displays an HTML page"""
+    states = storage.all(State).values()
+    sorted_
